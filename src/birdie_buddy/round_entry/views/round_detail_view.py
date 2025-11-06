eb0dd49f41e932320ce4ec783fb71b7091fd7319
Lee Sutton <leesutton1@gmail.com>
2025-11-05 19:03:48 -0800

    feat: add tiger 5 to the round details

--- src/birdie_buddy/round_entry/views/round_detail_view.py
+++ src/birdie_buddy/round_entry/views/round_detail_view.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
+from django.urls import reverse
 
 from birdie_buddy.round_entry.models import Round
+from birdie_buddy.round_entry.services.tiger_five import TigerFiveService
 
 
 class RoundDetailView(LoginRequiredMixin, View):
@@ -11,13 +13,14 @@ class RoundDetailView(LoginRequiredMixin, View):
         holes = round.hole_set.all()
 
         # Continue link: point to the first hole (number=1) for now.
-        from django.urls import reverse
-
-        continue_href = reverse("round_entry:create_hole", kwargs={"id": round.id, "number": 1})
+        continue_href = reverse(
+            "round_entry:create_hole", kwargs={"id": round.id, "number": 1}
+        )
 
         context = {
             "round": round,
             "holes": holes,
+            "tiger": TigerFiveService().get_for_round(round),
             "show_stats": round.complete,
             "continue_href": continue_href,
         }
